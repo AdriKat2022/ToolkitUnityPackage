faad6df21d8b275c3b320f6418e6839b22a24d22
AdriKat2022 <adrien.schroedel91@gmail.com>
2025-02-12 00:07:38 +0100

    forgot to put enum in namespace

--- Runtime/Scripts/CodePatterns/SingletonPolicy.cs
+++ Runtime/Scripts/CodePatterns/SingletonPolicy.cs
@@ -1,11 +1,14 @@
-public enum SingletonPolicy
+namespace AdriKat.Utils.CodePatterns
 {
-    /// <summary>
-    /// The first created instance remains, new ones get destroyed.
-    /// </summary>
-    FirstStays,
-    /// <summary>
-    /// The new instance replaces the old one.
-    /// </summary>
-    LastStays
+    public enum SingletonPolicy
+    {
+        /// <summary>
+        /// The first created instance remains, new ones get destroyed.
+        /// </summary>
+        FirstStays,
+        /// <summary>
+        /// The new instance replaces the old one.
+        /// </summary>
+        LastStays
+    }
 }
