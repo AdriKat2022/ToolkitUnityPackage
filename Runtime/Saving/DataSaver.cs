ef7ef0ae2001973712e75c9a3a79385a8e886ac8
AdriKat2022 <adrien.schroedel91@gmail.com>
2026-03-09 14:45:41 +0100

    added a runtime saving system

---
+++ Runtime/Saving/DataSaver.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace AdriKat.Toolkit.Saving
+{
+    /// <summary>
+    /// Ready-to-use quick saver utility to save and load serializable objects.
+    /// All the data is automatically stored within the persistent data folder (located at Application.persistentDataPath).
+    /// </summary>
+    /// <typeparam name="TData">The object to save/load. Must be serializable.</typeparam>
+    public static class DataSaver<TData> where TData : class
+    {
+        /// <summary>
+        /// Saves the data to the given filename.
+        /// If a file with the same filename already exists, it will be overwritten.
+        /// </summary>
+        /// <param name="data">The data to save.</param>
+        /// <param name="filename">The future data's location. Can contain slashes for subdirectories.</param>
+        public static void SaveData(TData data, string filename)
+        {
+            SaveUtility.SaveToBinary(data, SaveUtility.AppendToPersistentDataPath(filename));
+        }
+    
+        /// <summary>
+        /// Loads the TData object from the filename.
+        /// If the file doesn't exist, it will return null.
+        /// </summary>
+        /// <param name="filename">The data's location. Can contain slashes for subdirectories.</param>
+        public static TData LoadData(string filename)
+        {
+            return SaveUtility.LoadFromBinary<TData>(SaveUtility.AppendToPersistentDataPath(filename));
+        }
+        
+        /// <summary>
+        /// Loads the TData object from the filename.
+        /// If the file doesn't exist or the deserialization produces a null object, it will return the initializer's return value instead.
+        /// </summary>
+        /// <param name="filename">The data's location. Can contain slashes for subdirectories.</param>
+        /// <param name="initializer">A function that generates an initialized TData.</param>
+        /// <returns>The TData loaded from the filename, or the initializer's return value if the deserialization produced a null object.</returns>
+        public static TData LoadOrInitData(string filename, Func<TData> initializer)
+        {
+            return SaveUtility.LoadFromBinary<TData>(SaveUtility.AppendToPersistentDataPath(filename)) ?? initializer();
+        }
+
+        /// <summary>
+        /// Loads the TData object from the filename.
+        /// If the file doesn't exist or the deserialization produces a null object, it will return the defaultValue instead.
+        /// </summary>
+        /// <param name="filename">The data's location. Can contain slashes for subdirectories.</param>
+        /// <param name="defaultValue">The default value returned if the deserialization produces a null object.</param>
+        /// <returns>The TData loaded from the filename, or the defaultValue if the deserialization produced a null object.</returns>
+        public static TData LoadOrDefaultData(string filename, TData defaultValue)
+        {
+            return SaveUtility.LoadFromBinary<TData>(SaveUtility.AppendToPersistentDataPath(filename)) ?? defaultValue;
+        }
+
+        /// <summary>
+        /// Returns true if the filename exists or false otherwise.
+        /// </summary>
+        /// <param name="filename">The data's location to test the existence. Can contain slashes for subdirectories.</param>
+        /// <returns></returns>
+        public static bool Exists(string filename)
+        {
+            return File.Exists(SaveUtility.AppendToPersistentDataPath(filename));
+        }
+    }
+}
---
+++ Runtime/Saving/SaveUtility.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace AdriKat.Toolkit.Saving
+{
+    /// <summary>
+    /// Utility class to perform save and load operations.
+    /// </summary>
+    public static class SaveUtility
+    {
+        /// <summary>
+        /// Appends filepath to the persistentDataPath variable (Application.persistentDataPath).
+        /// </summary>
+        /// <param name="filepath">The path to append to the persistent path.</param>
+        public static string AppendToPersistentDataPath(string filepath)
+        {
+            return Application.persistentDataPath + "/" + filepath;
+        }
+        
+        /// <summary>
+        /// Loads an object from the provided fullFilepath file using binary deserialization.
+        /// </summary>
+        /// <param name="fullFilepath">The full path of the file to deserialize.</param>
+        /// <typeparam name="T">The object to deserialize.</typeparam>
+        /// <returns>The deserialized object T.</returns>
+        public static T LoadFromBinary<T>(string fullFilepath) where T : class
+        {
+            if (!File.Exists(fullFilepath)) return null;
+
+            try 
+            {
+                using FileStream file = new FileStream(fullFilepath, FileMode.OpenOrCreate);
+
+                if (file.Length <= 0)
+                {
+                    return null;
+                }
+                
+                BinaryFormatter formatter = new BinaryFormatter();
+                T playerDataSaveData = formatter.Deserialize(file) as T;
+                
+                file.Close();
+                
+                return playerDataSaveData;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                throw;
+            }
+        }
+        
+        /// <summary>
+        /// Saves an object to the provided fullFilepath file using binary serialization.
+        /// </summary>
+        /// <param name="data">The data to serialize.</param>
+        /// <param name="filepath">The path to save the file (file's name included).</param>
+        /// <typeparam name="T">The object type to serialize.</typeparam>
+        /// <exception cref="InvalidOperationException">Thrown if T is not serializable.</exception>
+        public static void SaveToBinary<T>(T data, string filepath) where T : class
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new InvalidOperationException("The type must be serializable.");
+            }
+            
+            Debug.Log($"Saving at '{filepath}'.");
+        
+            try
+            {
+                using FileStream file = new FileStream(filepath, FileMode.OpenOrCreate);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(file, data);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                throw;
+            }
+        }
+    }
+}
